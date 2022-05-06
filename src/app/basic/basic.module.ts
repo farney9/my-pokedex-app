8b74a2086f3b2eccc34291a38c895384a2dc01b2
Farney <farney9@gmail.com>
2022-05-06 11:21:09 -0500

    ng build in docs folder

--- src/app/basic/basic.module.ts
+++ src/app/basic/basic.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CounterComponent } from './counter/counter.component';
 import { CharizardComponent } from './charizard/charizard.component';
 
 
 
 @NgModule({
   declarations: [
-    CounterComponent,
     CharizardComponent
   ],
   imports: [
