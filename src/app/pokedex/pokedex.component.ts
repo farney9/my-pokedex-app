4d8ef3e4cde4afa4cc6bb30ce17933ac2ce00fd2
Farney <farney9@gmail.com>
2022-05-10 17:45:00 -0500

    Update data

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -11,14 +11,19 @@ export class PokedexComponent implements OnInit {
 
   poke?: Pokemon;
 
+  tipoPokemon: string = '';
+  ability: string = '';
+
   constructor( private readonly pokemonService:PokemonService) { }
 
   ngOnInit(): void {
     this.pokemonService.getPokemon(6)
       .subscribe(pokemon => {
         this.poke = pokemon
+        this.tipoPokemon = this.poke.types[0].type.name;
+        this.ability = this.poke.abilities[0].ability.name;
 
-        console.log(pokemon);
+        console.log(this.poke.types[0].type.name);
         
       })
   }
